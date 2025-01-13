3c5e66129e63ef7aa4a431d4c53d709daf45ab10
getmoin <heymoinn@gmail.com>
2025-01-13 10:21:57 -0500

    1.19.8

--- load-agent/locustMediatorIssueAIP2.py
+++ load-agent/locustMediatorIssueAIP2.py
@@ -17,15 +17,21 @@ class CustomLocust(User):
         self.client = CustomClient(self.host)
 
 class UserBehaviour(SequentialTaskSet):
+    # Class-level counters for all tasks
     credentials_issued = 0
+    invites_gotten = 0
+    invites_accepted = 0
     
     def on_start(self):
         print("Started process")
         self.client.startup(withMediation=bool(WITH_MEDIATION))
 
     def on_stop(self):
-        print("end process")
-        print(f"Total credentials issued in this session: {UserBehaviour.credentials_issued}")
+        print("\n=== Summary ===")
+        print(f"Invitations Retrieved: {UserBehaviour.invites_gotten}")
+        print(f"Invitations Accepted: {UserBehaviour.invites_accepted}")
+        print(f"Credentials Issued: {UserBehaviour.credentials_issued}")
+        print("===========================\n")
         self.client.shutdown()
         self.interrupt()
 
@@ -33,23 +39,22 @@ class UserBehaviour(SequentialTaskSet):
     def get_invite(self):
         invite = self.client.issuer_getinvite()
         self.invite = invite
+        UserBehaviour.invites_gotten += 1
 
     @task
     def accept_invite(self):
         self.client.ensure_is_running()
-
         connection = self.client.accept_invite(self.invite['invitation_url'])
         self.connection = connection
+        UserBehaviour.invites_accepted += 1
 
     @task
     def receive_cred_2_0(self):
         self.client.ensure_is_running()
-
         self.client.receive_credential_v_2_0(self.invite['connection_id'])
         UserBehaviour.credentials_issued += 1
 
 class Issue(CustomLocust):
     tasks = [UserBehaviour]
     wait_time = between(float(os.getenv('LOCUST_MIN_WAIT',0.1)), float(os.getenv('LOCUST_MAX_WAIT',1)))
-    fixed_count = 1
 #    host = "example.com"
